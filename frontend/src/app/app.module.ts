a4e7f779b002242f42ca60cb9b9e5522fd565e7f
Stefan Ganswint <stefan.ganswint@mni.thm.de>
2023-08-09 12:16:50 +0200

    Login started. TODO: Glas effect on Login form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StartseiteComponent } from './startseite/startseite.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartseiteComponent
+    StartseiteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
