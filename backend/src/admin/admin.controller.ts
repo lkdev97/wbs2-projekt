acd56f48a8adf90503964bea205200bcf2cf0221
Lars Köhler <lars.koehler@mni.thm.de>
2023-08-24 11:17:41 +0200

    sync

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, UseGuards} from '@nestjs/common';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+
+    @Get('editor')
+    editorPage() {
+        return "editor Page";
+    }
+    @Get()
+    adminPage() {
+        return "this is the admin page";
+    }
+}
