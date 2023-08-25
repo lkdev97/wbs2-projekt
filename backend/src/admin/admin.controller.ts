ceb19cea9a9c05a083a666201a8dd090d22035d4
Lars Köhler <lars.koehler@mni.thm.de>
2023-08-25 10:51:39 +0200

    Add Delete to Admin Editor

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, UseGuards, Param, Body, Post} from '@nestjs/common';
+import {Controller, Get, UseGuards, Param, Body, Post, Delete} from '@nestjs/common';
 import { AdminService } from "./admin.service";
 import { CreateQuestionDto } from "./dto/create-question";
 import { AdminGuard } from "./admin.guard";
@@ -28,6 +28,21 @@ export class AdminController {
     @Post('editor/create')
     async createQuestionById(@Body() createQuestionDto: CreateQuestionDto){
         return this.adminSerivce.createQuestion(createQuestionDto);
+    }
+
+    /**
+     *
+     * @param id
+     *
+     * Test with curl
+     *
+     * replace 1 with any valid 'id' from question_entity
+     *
+     * >curl -X DELETE http://localhost:3000/admin/editor/delete/1
+     */
+    @Delete('editor/delete/:id')
+    async deleteCardById(@Param('id') id: string) {
+        return this.adminSerivce.deleteQuestion(id);
     }
     @Get()
     adminPage() {
