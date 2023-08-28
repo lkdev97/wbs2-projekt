2ed2dda640fa23c6029e45d24a27e30cf9ba7982
dimas <dmytro.skorbyashchenskyy@mni.thm.de>
2023-08-28 20:31:21 +0200

    admin-view hinzugefügt

---
+++ frontend/src/app/admin-view/admin-view.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view',
+  templateUrl: './admin-view.component.html',
+  styleUrls: ['./admin-view.component.css']
+})
+export class AdminViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const availableQuestionsList = document.getElementById('availableQuestions');
+    const selectedQuestionsList = document.getElementById('selectedQuestions');
+
+    availableQuestionsList?.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.tagName === 'LI') {
+        target.classList.toggle('selected');
+        if (target.classList.contains('selected')) {
+          selectedQuestionsList?.appendChild(target);
+        } else {
+          availableQuestionsList.appendChild(target);
+        }
+      }
+    });
+
+    selectedQuestionsList?.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.tagName === 'LI') {
+        target.classList.toggle('selected');
+        if (target.classList.contains('selected')) {
+          availableQuestionsList?.appendChild(target);
+        } else {
+          selectedQuestionsList.appendChild(target);
+        }
+      }
+    });
+  }
+}
