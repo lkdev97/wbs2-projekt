406f236755455e03d4fd9e45ff24eb1202a6fe1c
dimas <dmytro.skorbyashchenskyy@mni.thm.de>
2023-09-02 03:54:14 +0200

    Spielerliste und Freundesliste hinzugefügt

--- frontend/src/app/profilseite/profilseite.component.ts
+++ frontend/src/app/profilseite/profilseite.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilseite.component.css']
 })
 export class ProfilseiteComponent {
+  // Testdaten
+  playersList = [
+    { name: 'Spieler 1', online: true },
+    { name: 'Spieler 2', online: false },
+    { name: 'Spieler 3', online: false },
+    { name: 'Spieler 4', online: false },
 
+  ];
+
+  friendsList = [
+    { name: 'Freund 1', online: true },
+    { name: 'Freund 2', online: false },
+
+  ];
+
+
+  addToFriendslist(player: any) {
+
+  }
+
+
+  deleteFriend(friend: any) {
+
+  }
+
+
+  startDuell(friend: any) {
+
+  }
 }
