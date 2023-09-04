7948fe80fa452e0d2205d556be3bb107237f6a39
dimas <dmytro.skorbyashchenskyy@mni.thm.de>
2023-09-04 05:30:21 +0200

    Dummyfunktionen für Spieler- und Freundesliste hinzugefügt.

--- frontend/src/app/profilseite/profilseite.component.ts
+++ frontend/src/app/profilseite/profilseite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-profilseite',
@@ -6,33 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./profilseite.component.css']
 })
 export class ProfilseiteComponent {
+
   // Testdaten
-  playersList = [
-    { name: 'Spieler 1', online: true },
-    { name: 'Spieler 2', online: false },
-    { name: 'Spieler 3', online: false },
-    { name: 'Spieler 4', online: false },
+   playersList = [
+    {name: 'Spieler 1', online: true},
+    {name: 'Spieler 2', online: false},
+    {name: 'Spieler 3', online: false},
+    {name: 'Spieler 4', online: false},
 
   ];
 
-  friendsList = [
-    { name: 'Freund 1', online: true },
-    { name: 'Freund 2', online: false },
+   friendsList = [
+    {name: 'Freund 1', online: true},
+    {name: 'Freund 2', online: false},
 
   ];
-
-
   addToFriendslist(player: any) {
-
+    // Prüfen, ob der Spieler bereits in der Freundesliste ist
+    if (!this.isPlayerInFriendsList(player)) {
+      // Spieler zur Freundesliste hinzufügen
+      this.friendsList.push(player);
+    }
   }
 
 
   deleteFriend(friend: any) {
-
+    // Den Index des Freundes in der Freundesliste finden
+    const index = this.friendsList.indexOf(friend);
+    // Prüfen, ob der Freund in der Liste ist
+    if (index !== -1) {
+      // Freund aus der Freundesliste entfernen
+      this.friendsList.splice(index, 1);
+    }
   }
 
+  private isPlayerInFriendsList(player: any): boolean {
+    return this.friendsList.some((friend) => friend.name === player.name);
+  }
 
-  startDuell(friend: any) {
+  startDuel(friend: any) {
 
   }
 }
