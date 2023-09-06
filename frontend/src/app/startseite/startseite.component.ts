324478391f6a1729333173cd5508b4400c157124
Stefan Ganswint <stefan.ganswint@mni.thm.de>
2023-09-06 12:38:10 +0200

    Start und End duel Button auf der Startseite eingebunden (TEST) diese Funktionieren auch solang ich die Ids der Nutzer hardcode. Wird verändert wenn die Freundesliste fertig ist

--- frontend/src/app/startseite/startseite.component.ts
+++ frontend/src/app/startseite/startseite.component.ts
@@ -19,6 +19,27 @@ export class StartseiteComponent implements OnInit{
     this.route.navigate(['/login'])
   }
 
+  duelstart(){
+    console.log("start")
+    this.http.post<any>('http://localhost:3000/duel',
+      {challengerId: "ae3a6a2c-4ca9-4539-bfd8-dad4babdffd3", opponentId: "4d08c859-4cb5-4b65-bbde-ebd1f1649780"})
+      .subscribe(data =>{
+
+      console.log(data)
+    })
+
+  }
+  duelend(){
+    console.log("ende")
+    this.http.patch<any>('http://localhost:3000/duel/update',
+      {duelId: "dafe1839-e5ac-4258-98fd-af420a24aeb5", questionId: "ques567", answer: "Paris"})
+      .subscribe(data =>{
+
+      console.log(data)
+    })
+
+  }
+
 
   ngOnInit() {
     this.http.get<any>(`http://localhost:3000/auth/user`).subscribe(data => {
