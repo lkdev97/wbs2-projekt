162128dbc2e2aee7a84eb6cd13f8a0d8f9002939
Lars Köhler <lars.koehler@mni.thm.de>
2023-09-16 22:20:40 +0200

    Merge lars-dev

--- backend/src/question/question.controller.ts
+++ backend/src/question/question.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { QuestionService } from './question.service';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('question')
 @ApiTags('question')
-export class QuestionController {}
+export class QuestionController {
+
+    constructor(private readonly questionSerive: QuestionService) {}
+    @Get(':id')
+    getQuestionById(@Param('id') id: string) {
+        return this.questionSerive.getQuestionById(id);
+    }
+}
--- backend/src/question/question.service.ts
+++ backend/src/question/question.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { QuestionEntity } from './entities/questionEntity.entity';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+    constructor(
+        @InjectRepository(QuestionEntity)
+        private questionRepository: Repository<QuestionEntity>,
+      ) {}
+
+    getQuestionById(id: string) {
+        return this.questionRepository.findOne({where: {id}});
+    }
+}
