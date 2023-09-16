effc3e48415dd4e2030d6890d0b9bde0da8b62d8
Lars Köhler <lars.koehler@mni.thm.de>
2023-09-16 22:22:57 +0200

    Merge lars-dev

--- backend/src/question/question.controller.ts
+++ backend/src/question/question.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { QuestionService } from './question.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags,    
+    ApiOperation,
+    ApiOkResponse,
+    ApiCreatedResponse, } from '@nestjs/swagger';
 
 @Controller('question')
 @ApiTags('question')
 export class QuestionController {
 
     constructor(private readonly questionSerive: QuestionService) {}
+
+    @ApiOperation({ summary: 'Get question by ID' })
+    @ApiOkResponse({ description: 'Successfully returned the question' })
     @Get(':id')
     getQuestionById(@Param('id') id: string) {
         return this.questionSerive.getQuestionById(id);
